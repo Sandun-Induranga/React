8c749df062f3d4350d9f6e166fd760d7e80d2d7d
Sandun Induranga <sandutharu40@gmail.com>
2023-03-18 08:58:48 +0530

    Content Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
-  componentDidMount(): void {
-    alert("Hello World");
-  }
+  // componentDidMount(): void {
+  //   alert("Hello World");
+  // }
 
   render(): React.ReactNode {
     return (
-      <div className="App">
-        <h1>Hello React</h1>
-        {/* <span className="inline-grid grid-cols-3 gap-4">
-          <span>01</span>
-          <span>02</span>
-          <span>03</span>
-          <span>04</span>
-          <span>05</span>
-          <span>06</span>
-        </span>
-        <span className="inline-grid grid-cols-3 gap-4">
-          <span>01</span>
-          <span>02</span>
-          <span>03</span>
-          <span>04</span>
-          <span>05</span>
-          <span>06</span>
-        </span> */}
+      <div className="bg-blue-edited-200">
+        <h1 className="text-center font-light min-h-screen">Hello World</h1>
       </div>
     );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    ".src/**/*.{js, jsx, ts, tsx}" /* Apply Tailwind for these contents  */
+    "./src/**/*.{js, jsx, ts, tsx}" /* Apply Tailwind for these contents  */
   ],
   theme: {
-    extend: {},
-    colors:{
-      "blue-edited":{
-        300:"#03045e",
-        200:"#023e8a"
+    extend: {
+      colors:{
+        "blue-edited":{
+          300:"#03045e",
+          200:"#023e8a"
+        }
       }
-    }
+    },
   },
   plugins: [],
 }

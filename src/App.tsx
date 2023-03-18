590b6ec41e5df758aed5c770f2513e0c6a364a38
Sandun Induranga <sandutharu40@gmail.com>
2023-03-18 14:39:36 +0530

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
+import Content from "./components/Content";
 import Counter from "./components/Counter";
+import Header from "./components/Header";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
@@ -14,11 +16,13 @@ export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="min-h-screen">
+        <Header />
+        <Content />
         {/* <Home />
         <About />
         <Contact />
         <Profile /> */}
-        <Counter />
+        {/* <Counter /> */}
       </div>
     );
   }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="bg-blue-edited-300 text-white fixed w-full h-20 top-0">
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}

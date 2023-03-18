61eec830e97b71a7382010666d4ff626533ca627
Sandun Induranga <sandutharu40@gmail.com>
2023-03-18 09:10:44 +0530

    Content Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js, jsx, ts, tsx}" /* Apply Tailwind for these contents  */
+    "./src/**/*.{js,jsx,ts,tsx}" /* Apply Tailwind for these contents  */
   ],
   theme: {
     extend: {

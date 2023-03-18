ec6a1a4927fe7c96bbf7fb7816fb3fe4241e8aec
Sandun Induranga <sandutharu40@gmail.com>
2023-03-18 13:29:46 +0530

    Counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,16 +1,46 @@
+import { type } from "os";
 import React, { Component } from "react";
 
-export default class Counter extends Component {
+type CounterProps = {};
+
+type CounterState = {
+  count: number;
+};
+
+export default class Counter extends Component<CounterProps, CounterState> {
+  constructor(props: CounterProps) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decreaseCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <div className="p-5 min-h-screen flex justify-center items-center flex-col">
         <div className="shadow-xl p-40 rounded-xl">
-          <h1 className="text-center font-bold text-6xl">Count : 0</h1>
+          <h1 className="text-center font-bold text-6xl">
+            Count : {this.state.count}
+          </h1>
           <div className="flex space-x-4 mt-20 justify-center">
-            <button className="border text-green-800 border-green-800 rounded-xl hover:bg-green-800 hover:text-white py-2 px-4">
+            <button
+              className="border text-green-800 border-green-800 rounded-xl hover:bg-green-800 hover:text-white py-2 px-4"
+              onClick={this.increaseCount}
+            >
               Increase
             </button>
-            <button className="border text-red-800 border-red-800 rounded-xl hover:bg-red-800 hover:text-white py-2 px-4">
+            <button
+              className="border text-red-800 border-red-800 rounded-xl hover:bg-red-800 hover:text-white py-2 px-4"
+              onClick={this.decreaseCount}
+            >
               Decrease
             </button>
           </div>

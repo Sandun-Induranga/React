fa29915e10591dcb3c321f33d178cc102e2f0753
Sandun Induranga <sandutharu40@gmail.com>
2023-03-25 13:27:42 +0530

    About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import AboutIntro from "../../components/AboutIntro";
+import CardContainer from "../../components/CardContainer";
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <AboutIntro />
+        <CardContainer />
       </div>
     );
   }

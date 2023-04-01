895a15b50aa2e0880729004088ae7dc86c921727
Sandun Induranga <sandutharu40@gmail.com>
2023-04-01 12:51:11 +0530

    Profile Updated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -7,12 +7,14 @@ import Post from "../../components/Post";
 type ProfileProps = {};
 type ProfileState = {
   posts: PostDetail[];
+  isClickedCreateNewPost: boolean;
 };
 
 export default class Profile extends Component<ProfileProps, ProfileState> {
   constructor(props: ProfileProps) {
     super(props);
     this.state = {
+      isClickedCreateNewPost: false,
       posts: [
         {
           id: "1",
@@ -55,6 +57,22 @@ export default class Profile extends Component<ProfileProps, ProfileState> {
       <>
         <Header />
         <div className="p-6">
+          <div className="w-full flex flex-col">
+            {!this.state.isClickedCreateNewPost ? (
+              <>
+                <div className="cursor-pointer w-full p-4 bg-blue-900 text-white rounded">
+                  <h6>Create New Post</h6>
+                </div>
+                <div className="flex justify-center items-center cursor-pointer w-full py-6 px-4 rounded border border-slate-400 text-slate-400">
+                  <h6>Your New Post</h6>
+                </div>
+              </>
+            ) : (
+              <div className="cursor-pointer w-full p-4 bg-blue-900 text-white rounded">
+                <h6>Discart Post</h6>
+              </div>
+            )}
+          </div>
           <div className="p-6">
             {this.state.posts.map((post) => (
               <Post
